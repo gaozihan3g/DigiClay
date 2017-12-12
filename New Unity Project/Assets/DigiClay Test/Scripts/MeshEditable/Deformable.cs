db10822993c2347c41dd235492d98200b06c8347
gaozihan3g <godshand@Zihans-MacBook-Pro.local>
2017-12-12 13:33:33 +0800

    deformable and cursor

--- New Unity Project/Assets/DigiClay Test/Scripts/MeshEditable/Deformable.cs
+++ New Unity Project/Assets/DigiClay Test/Scripts/MeshEditable/Deformable.cs
@@ -11,18 +11,26 @@ public class Deformable : MonoBehaviour
     , IEndDragHandler
 {
 	[Range(0.01f, 1f)]
-    public float _radius = 0.5f;
+	public float _innerRadius = 0.1f;
+
 	[Range(0.01f, 1f)]
+    public float _outerRadius = 0.5f;
+
+	[Range(0f, 1f)]
     public float _strength = 0.1f;
+
 	public bool _symmetric = true;
 	public bool _push = true;
 
     MeshFilter _meshFilter;
     MeshCollider _meshCollider;
 
+	Vector3 _beginDragPosition;
+	Vector3[] originalVertices;
+
+
 	void Awake()
 	{
-//		_advMeshContext = GetComponent<AdvancedMeshContext> ();
 		_meshFilter = GetComponentInChildren<MeshFilter>();
 		_meshCollider = GetComponentInChildren<MeshCollider>();
 	}
@@ -36,8 +44,15 @@ public class Deformable : MonoBehaviour
     {
 		if (SystemManager.Instance.Mode != SystemManager.EditMode.Sculpture)
 			return;
-		
-        Debug.Log("Deformation begins!");
+
+		// get original drag position
+		_beginDragPosition = eventData.pointerCurrentRaycast.worldPosition;
+
+		// save original vertices
+		//TODO only update outer side vertices
+		originalVertices = _meshFilter.mesh.vertices;
+
+		Debug.Log("Deformation begins! " + _beginDragPosition);
     }
 
 	public void OnDrag(PointerEventData eventData)
@@ -45,38 +60,29 @@ public class Deformable : MonoBehaviour
 		if (SystemManager.Instance.Mode != SystemManager.EditMode.Sculpture)
 			return;
 
+		Vector3 dragOffset = eventData.pointerCurrentRaycast.worldPosition - _beginDragPosition;
+
+
 		Vector3[] vertices = _meshFilter.mesh.vertices;
 
         for (int i = 0; i < vertices.Length; ++i)
         {
 			float dist = 0f;
 
-			dist = _symmetric ?
-				Mathf.Abs (vertices [i].y - eventData.pointerCurrentRaycast.worldPosition.y) : 
-				Vector3.Distance (vertices [i], eventData.pointerCurrentRaycast.worldPosition);
+			dist = Vector3.Distance (vertices [i], eventData.pointerCurrentRaycast.worldPosition);
 
-			if (dist < _radius)
-			{
-				Vector3 radialVec = new Vector3 (vertices [i].x, 0, vertices [i].z);
 
-				if (radialVec.sqrMagnitude > (DigiClayConstant.MAX_RADIUS * DigiClayConstant.MAX_RADIUS) ||
-					radialVec.sqrMagnitude < (DigiClayConstant.MIN_RADIUS * DigiClayConstant.MIN_RADIUS))
-					break;
+			vertices[i] = originalVertices[i] + dragOffset * _strength * Falloff(_innerRadius, _outerRadius, dist);
 
-				Vector3 radialUnitDir = radialVec.normalized;
-
-				int outerSign = Vector3.Angle (eventData.pointerCurrentRaycast.worldNormal, radialUnitDir) < 90 ? 1 : -1;
-
-				int pushSign = _push ? -1 : 1;
-
-				if (_symmetric)
-					vertices[i] += radialUnitDir * _strength * Time.deltaTime * Falloff(dist, _radius) * pushSign;
-				else
-					vertices[i] += radialUnitDir * _strength * Time.deltaTime * Falloff(dist, _radius) * pushSign * outerSign;
-			}
+			//TODO symmetric deform
         }
 		_meshFilter.mesh.vertices = vertices;
 		_meshFilter.mesh.RecalculateNormals ();
+
+
+		//bad
+		_meshCollider.sharedMesh = _meshFilter.mesh;
+		Debug.Log("# Deforming! " + dragOffset);
     }
 
 
@@ -95,9 +101,84 @@ public class Deformable : MonoBehaviour
 
     }
 
-	float Falloff(float dist, float radius)
+	float Falloff(float t)
 	{
 		//        return Mathf.Clamp01(1f - dist / radius);
-		return Mathf.Clamp01 (Mathf.Pow (360.0f, -Mathf.Pow (dist / radius, 2.5f) - 0.01f));
+		return Mathf.Clamp01 (Mathf.Pow (360.0f, -Mathf.Pow (t, 2.5f) - 0.01f));
+	}
+
+	float Falloff(float inner, float outer, float value)
+	{
+		if (value < inner)
+			return 1f;
+		if (value > outer)
+			return 0f;
+
+		//linear
+		return Mathf.InverseLerp (inner, outer, value);
 	}
 }
+
+
+//public void OnBeginDrag(PointerEventData eventData)
+//{
+//	if (SystemManager.Instance.Mode != SystemManager.EditMode.Sculpture)
+//		return;
+//
+//	Debug.Log("Deformation begins!");
+//}
+//
+//public void OnDrag(PointerEventData eventData)
+//{
+//	if (SystemManager.Instance.Mode != SystemManager.EditMode.Sculpture)
+//		return;
+//
+//	Vector3[] vertices = _meshFilter.mesh.vertices;
+//
+//	for (int i = 0; i < vertices.Length; ++i)
+//	{
+//		float dist = 0f;
+//
+//		dist = _symmetric ?
+//			Mathf.Abs (vertices [i].y - eventData.pointerCurrentRaycast.worldPosition.y) : 
+//			Vector3.Distance (vertices [i], eventData.pointerCurrentRaycast.worldPosition);
+//
+//		if (dist < _radius)
+//		{
+//			Vector3 radialVec = new Vector3 (vertices [i].x, 0, vertices [i].z);
+//
+//			if (radialVec.sqrMagnitude > (DigiClayConstant.MAX_RADIUS * DigiClayConstant.MAX_RADIUS) ||
+//				radialVec.sqrMagnitude < (DigiClayConstant.MIN_RADIUS * DigiClayConstant.MIN_RADIUS))
+//				break;
+//
+//			Vector3 radialUnitDir = radialVec.normalized;
+//
+//			int outerSign = Vector3.Angle (eventData.pointerCurrentRaycast.worldNormal, radialUnitDir) < 90 ? 1 : -1;
+//
+//			int pushSign = _push ? -1 : 1;
+//
+//			if (_symmetric)
+//				vertices[i] += radialUnitDir * _strength * Time.deltaTime * Falloff(dist, _radius) * pushSign;
+//			else
+//				vertices[i] += radialUnitDir * _strength * Time.deltaTime * Falloff(dist, _radius) * pushSign * outerSign;
+//		}
+//	}
+//	_meshFilter.mesh.vertices = vertices;
+//	_meshFilter.mesh.RecalculateNormals ();
+//}
+//
+//
+//
+//public void OnEndDrag(PointerEventData eventData)
+//{
+//	if (SystemManager.Instance.Mode != SystemManager.EditMode.Sculpture)
+//		return;
+//
+//	Debug.Log("Deformation ends!");
+//	_meshCollider.sharedMesh = _meshFilter.mesh;
+//}
+//float Falloff(float dist, float radius)
+//{
+//	//        return Mathf.Clamp01(1f - dist / radius);
+//	return Mathf.Clamp01 (Mathf.Pow (360.0f, -Mathf.Pow (dist / radius, 2.5f) - 0.01f));
+//}
