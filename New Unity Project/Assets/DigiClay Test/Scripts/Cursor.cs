37dace86409408a00bcb01ef9c5c5ccf7c30d1ad
gaozihan3g <godshand@Zihans-MacBook-Pro.local>
2017-12-13 14:54:02 +0800

    cursor script

---
+++ New Unity Project/Assets/DigiClay Test/Scripts/Cursor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DigiClay
+{
+	public class Cursor : MonoBehaviour {
+
+		[Range(DigiClayConstant.MIN_RADIUS, DigiClayConstant.MAX_RADIUS)]
+		public float outerRadius;
+
+		[Range(0f, 1f)]
+		public float innerRatio;
+
+
+		public Transform outerSphere;
+		public Transform innerSphere;
+
+		public Slider outerSlider;
+		public Slider innerSlider;
+
+
+		void Start()
+		{
+			if (innerSlider != null)
+			{
+				innerSlider.value = innerRatio;
+				innerSlider.onValueChanged.AddListener (UpdateInnerRadius);
+			}
+
+			if (outerSlider != null)
+			{
+				outerSlider.value = Mathf.InverseLerp (DigiClayConstant.MIN_RADIUS, DigiClayConstant.MAX_RADIUS, innerRatio);
+				outerSlider.onValueChanged.AddListener (UpdateOuterRadius);
+			}
+		}
+
+		public void UpdateOuterRadius(float v)
+		{
+			outerRadius = Mathf.Lerp (DigiClayConstant.MIN_RADIUS, DigiClayConstant.MAX_RADIUS, v);
+			outerSphere.localScale = Vector3.one * outerRadius;
+
+			innerSphere.localScale = Vector3.one * outerRadius * innerRatio;
+		}
+
+		public void UpdateInnerRadius(float v)
+		{
+			innerRatio = v;
+			innerSphere.localScale = Vector3.one * outerRadius * innerRatio;
+		}
+	}
+}
