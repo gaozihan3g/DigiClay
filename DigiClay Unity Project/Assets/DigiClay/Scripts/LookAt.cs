c8ff51c3b480b08a06de4c17acda89d6eeb42279
gaozihan3g <gao_zihan@126.com>
2018-06-07 15:17:04 +0800

    icon updated

---
+++ DigiClay Unity Project/Assets/DigiClay/Scripts/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LookAt : MonoBehaviour {
+
+    public Transform m_target;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (m_target != null)
+            transform.LookAt(m_target);
+	}
+}
